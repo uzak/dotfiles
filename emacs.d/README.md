7cc558d1996d9310c058cf346fd393b130d92165
Martin Užák <martin.uzak@gmail.com>
2020-10-23 08:35:43 +0200

    add emacs stuff

---
+++ emacs.d/README.md
@@ -0,0 +1,2 @@
+Source: https://www.youtube.com/feeds/videos.xml?channel_id=UCxkMDXQ5qzYOgXPRnOBrp1w   # Mike Zamansky
+
---
+++ emacs.d/samples/python/test.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+alfa = 20
+
+print ("hello")
+
+def f(a):
+    a += 1
+    random.randint(1, 2)
