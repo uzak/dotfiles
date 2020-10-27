0258fdbbf55bdc1191eb19b3f3a5ac10cc312f58
Martin Užák <martin.uzak@gmail.com>
2020-10-27 09:14:59 +0100

    new password

--- bin/new_connect_db.sh
+++ bin/new_connect_db.sh
@@ -13,7 +13,7 @@ echo "drop schema $DB; create schema $DB; " | mysql -h $HOST -u $USER -p$PW $DB
 )
 
 mysql -h $HOST -u $USER -p$PW $DB <<EOF
-    insert into user(username,first_name,last_name,is_admin,password) values('martin', 'm', 'u', 1, 'a1333303f02ff10314ed48cefac4271194a2cb0d9d22ca570ebd641ca56c565dcf5b5834c1231827493c6a7c65aed045acb60e21182ecea734dda66ecfb28c038e5edd5a9b6d54e6adc0fe7d6de6e03ce41bfacbba6799b643c2149553f3a8db');
+    insert into user(username,first_name,last_name,is_admin,password) values('martin', 'm', 'u', 1, '95398920532e39f045d4faf3da7acddfb5ab12bf9db36c9fa49c5e5e2b0d7a42a1226f39bbf7603a41eade1b1f74937e9d440b28105588bf22ce1f655004b21ca396c9638ccef3716e8ec472f9761b99f96d2cf22a659840d1d00e23414b0f7f');
     insert into team(name) values ('martin');
     update user set default_team_id=(select id from team where name='martin') where username='martin';
     insert into user_team(user_id, team_id, rights_ro, rights_rw, rights_use) values((select id from user where username='martin'), (select id from team where name='martin'), 1, 1, 1);
